021a2a83f82446600af70222a9009e6a3ada5645
Aurelius <aurelius.dev@gmail.com>
2021-04-29 21:03:10 +0200

    Use module interface

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function InstantExit( mod ) {
+module.exports.NetworkMod = function InstantExit( mod ) {
 
     mod.hook( "S_PREPARE_EXIT", 1, () => {
         mod.toClient( "S_EXIT", 3, { category: 0, code: 0 });
